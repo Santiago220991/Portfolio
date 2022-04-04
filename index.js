3bada93fc202a21e1ec85a53534d52e792df954c
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-04 17:38:31 -0500

    Make scroll bar quit button close the bar

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const togglebutton=document.querySelector(".menu-mobile img")
 const s_bar=document.querySelector(".scroll-bar")
-console.log(s_bar)
-console.log(togglebutton)
+const quit_button=document.querySelector(".scroll-bar-image img")
 togglebutton.addEventListener("click", () => {
     s_bar.classList.toggle("active")
     console.log(togglebutton)
+})
+
+quit_button.addEventListener("click", () => {
+    s_bar.classList.remove("active")
 })
