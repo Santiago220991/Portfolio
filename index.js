0273ba893f398ecfef66e0cc48cd9ecba5138e3b
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-04 18:28:59 -0500

    Install javascript linters

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const togglebutton=document.querySelector(".menu-mobile img")
-const s_bar=document.querySelector(".scroll-bar")
-const quit_button=document.querySelector(".scroll-bar-image img")
-togglebutton.addEventListener("click", () => {
-    s_bar.classList.toggle("active")
-    console.log(togglebutton)
-})
+const togglebutton = document.querySelector('.menu-mobile img');
+const sbar = document.querySelector('.scroll-bar');
+const quitbutton = document.querySelector('.scroll-bar-image img');
+const sbarbuttons = document.querySelectorAll('.scroll-bar-list');
+const bmenu=document.querySelector('.menu');
 
-quit_button.addEventListener("click", () => {
-    s_bar.classList.remove("active")
-})
+togglebutton.addEventListener('click', () => {
+  sbar.classList.toggle('active');
+  bmenu.classList.toggle('active');
+});
+
+quitbutton.addEventListener('click', () => {
+  sbar.classList.remove('active');
+  bmenu.classList.toggle('active');
+});
+
+sbarbuttons.forEach(element => {
+    element.addEventListener('click', ()  =>{
+        sbar.classList.remove('active')
+        bmenu.classList.toggle('active');
+})})
