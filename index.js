9ab269e29f231613ccb865f4733a2531c9baf584
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-04 18:41:04 -0500

    Check linters in html css and js files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const togglebutton = document.querySelector('.menu-mobile img');
 const sbar = document.querySelector('.scroll-bar');
 const quitbutton = document.querySelector('.scroll-bar-image img');
 const sbarbuttons = document.querySelectorAll('.scroll-bar-list');
-const bmenu=document.querySelector('.menu');
+const bmenu = document.querySelector('.menu');
 
 togglebutton.addEventListener('click', () => {
   sbar.classList.toggle('active');
@@ -14,8 +14,9 @@ quitbutton.addEventListener('click', () => {
   bmenu.classList.toggle('active');
 });
 
-sbarbuttons.forEach(element => {
-    element.addEventListener('click', ()  =>{
-        sbar.classList.remove('active')
-        bmenu.classList.toggle('active');
-})})
+sbarbuttons.forEach((element) => {
+  element.addEventListener('click', () => {
+    sbar.classList.remove('active');
+    bmenu.classList.toggle('active');
+  });
+});
